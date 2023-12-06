8add4baf00296ef01bb8af27ce9ec3669bbf3ab6
NailLegProcessorDivide <github@whytho.com>
2023-12-06 07:55:16 +0000

    start day4

---
+++ day4/day4a.lisp
@@ -0,0 +1,21 @@
+(defun get-file (filename)
+    (with-open-file (stream filename)
+        (loop for line = (read-line stream nil)
+            while line
+            collect line)))
+
+(defun splitmi (del inp)
+    (if (position del inp)
+        (cons (subseq inp 0 (position del inp)) (splitmi del (subseq inp (+ (position del inp) 1))))
+        (list inp)))
+
+(defun parseParts (line)
+    (print line))
+
+(defun score (line)
+    (parseParts (splitmi #\| (nth 1 (splitmi #\colon line)))))
+
+(defun acc-lines (lines)
+    (reduce '+ (loop for line in lines collect (score line))))
+
+(print (acc-lines (get-file "input.txt")))
